f3fdfbf0ca04cab679906705c321edfc97c6a3ec
alber <albert.mazuz@gmail.com>
2024-06-03 18:52:11 -0300

    Criacao de arquivo config_template

---
+++ config_template.py
@@ -0,0 +1,10 @@
+import os
+#True para ambiente de desenvolvimente
+## False para ambiente de produção
+dev=False
+
+
+
+### URLs de Produção. Substitua pela IP do servidor de produção
+url='http://000.0.0.0:5000/identify'
+url_test='http://000.0.0.0:5000/test'
