b26c5bbd44e38ce4a6948628a03c3c0281d9b387
alber <albert.mazuz@gmail.com>
2024-06-03 18:34:38 -0300

    Adição de URL de produção em Config.py

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 dev=False
 
 
-if dev:
-    # Caminhos de modelos
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    id_recognition_model_path = os.path.join(current_dir, f"face_module\identification\modelos\id_recognition_model.xml")
-    landmark_model_path = os.path.join(current_dir, "face_module\liveness\shape_predictor_68_face_landmarks.dat")
-    classificador_68_path = landmark_model_path
+
+### IPs de Produção
+url='http://127.0.0.1:5000/identify'
+url_test='http://127.0.0.1:5000/test'
